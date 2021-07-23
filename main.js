b539ed07a5a401ec34723a15882cbd8e52c4cf57
Poshika-alt <76203042+Poshika-alt@users.noreply.github.com>
2021-07-23 21:41:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,550);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+
+function draw(){
+    background("#f38181");
+}
+
+function modelLoaded(){
+    console.log('Posenet is initialized');
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+}
+}
